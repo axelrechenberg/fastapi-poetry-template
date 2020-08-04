b665a552fdc758ace2d08d7117c59626ac93c842
Axel Rechenberg <axelreche@gmail.com>
2020-08-04 21:52:01 +0200

    Changed the example endpoint and response to a hello world example.

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def root(item_id: int):
-    return {"item_id": item_id}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
